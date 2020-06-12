b3787b4b3dde5019b669b1df3cae276d7f3b7dd7
wenyi <tianyipc@gmail.com>
2020-06-12 17:36:23 +0800

    ws内容和设置完毕 新建ws控制器

--- config/routes.php
+++ config/routes.php
@@ -12,4 +12,7 @@ declare(strict_types=1);
 
 use Hyperf\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+//Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+Router::addServer('ws', function () {
+    Router::get('/im', 'App\Controller\WebSocketController');
+});
